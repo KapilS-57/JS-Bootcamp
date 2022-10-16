1b7737da2951573d935ea6d73fcd18e21b49147a
Kapil Sharma <capishr57@gmail.com>
2022-10-16 21:39:51 +0530

    Added Assignment folder

---
+++ Assignments/01-Fundamentals-Part-1/starter/data-types.js
@@ -0,0 +1,4 @@
+
+
+// Assignment LECTURE: Values and Variables
+
---
+++ Assignments/01-Fundamentals-Part-1/starter/value-variables.js
@@ -0,0 +1,11 @@
+
+
+// Assignement LECTURE: Values and Variables
+
+var country = 'India';
+var continent = 'Asia';
+var population = '1380M (2020)';
+
+console.log('Country : ' + country);
+console.log('Continent : ' + continent);
+console.log('Population : ' + population);
